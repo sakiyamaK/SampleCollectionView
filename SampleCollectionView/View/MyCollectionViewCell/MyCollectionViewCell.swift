aec3e3c30b765748f596a6a31343b360afdfe771
sakiyamaK <sakiyama.k@gmail.com>
2020-06-07 15:34:19 +0900

    a

---
+++ SampleCollectionView/View/MyCollectionViewCell/MyCollectionViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  MyCollectionViewCell.swift
+//  SampleCollectionView
+//
+//  Created by sakiyamaK on 2020/06/07.
+//  Copyright © 2020 sakiyamaK. All rights reserved.
+//
+
+import Foundation
---
+++ SampleCollectionView/ViewController/Sample4/Sample4CollectionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  Sample4CollectionViewController.swift
+//  SampleCollectionView
+//
+//  Created by sakiyamaK on 2020/06/07.
+//  Copyright © 2020 sakiyamaK. All rights reserved.
+//
+
+import Foundation
